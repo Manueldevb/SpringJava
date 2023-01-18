35ff54b1d5a4f047869b0aa3cfb23550590c81ef
Manueldevb <manueldelosriosb@gmail.com>
2023-01-18 18:17:51 -0500

    video 20

--- src/main/java/com/platzi/market/PlatziMarketApplication.java
+++ src/main/java/com/platzi/market/PlatziMarketApplication.java
@@ -11,4 +11,3 @@ public class PlatziMarketApplication {
 	}
 
 }
-Este es una prueba
---
+++ src/main/java/com/platzi/market/persistance/ProductoRepository.java
@@ -0,0 +1,22 @@
+package com.platzi.market.persistance;
+
+import com.platzi.market.persistance.crud.ProductoCrudRepository;
+import com.platzi.market.persistance.entity.Producto;
+
+import java.util.List;
+import java.util.Optional;
+
+public class ProductoRepository {
+    private ProductoCrudRepository productoCrudRepository;
+    public List<Producto> getAll() {
+        return (List<Producto>) productoCrudRepository.findAll();
+    }
+    public List<Producto> getByCategoria(int idCategoria){
+        return productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
+
+    }
+
+    public Optional <List<Producto>> getEscasos (int cantidad, boolean estado){
+        return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
+    }
+}
